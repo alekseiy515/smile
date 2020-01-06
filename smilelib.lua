3666e4b7bbb46a052e375aed6c3d838b92cc7381
alekseiytalanov <alekseiy150@gmail.com>
2020-01-06 18:18:57 +0500

    Update smilelib.lua

--- smilelib.lua
+++ smilelib.lua
@@ -1,6 +1,12 @@
 --Main smile lib for boot the smile
+local nocontrol = false --Disable the control of the smile
 clear()
 cprint("Booting smile lib")
 dofile("smiletextinglib.lua") --init texting lib
 dofile("smilegraphicslib.lua") --init graphics lib
+if not nocontrol then
 dofile("smilekeyboardlib.lua") --init keyboard controls
+else
+color(8)
+print("The smile control is disabled you can only view the smile as picture")
+end
